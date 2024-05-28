da1b4358db94ae0561a4e9559d1a102356ee236f
buksabrina <87337146+buksabrina@users.noreply.github.com>
2024-05-28 16:47:56 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us, ' + document.getElementById('name').value + '!');
+    document.getElementById('contactForm').reset();
+});
